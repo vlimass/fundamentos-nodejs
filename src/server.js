f2ef51ef5b33dced47ebafac4bb0dcfb9686144f
Vinícius Lima <villylss@gmail.com>
2023-10-18 20:37:59 -0300

    camada de persistência de dados adicionada com o banco de dados

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
 import { json } from './middlewares/json.js'
+import { Database } from './database.js'
 
-const users = []
+// UUID => Universally Unique Identifier
+
+const database = new Database()
 
 const server = http.createServer(async (request, response) =>  {
   const { method, url } = request
@@ -9,18 +13,21 @@ const server = http.createServer(async (request, response) =>  {
   await json(request, response)
 
   if(method === 'GET' && url === '/users') {
-    return response
-      .end(JSON.stringify(users))
+    const users = database.select('users')
+
+    return response.end(JSON.stringify(users))
   }  
 
   if(method === 'POST' && url === '/users') {
     const { name, email } = request.body
 
-    users.push({
-      id: 1,
+    const user = {
+      id: randomUUID(),
       name,
       email
-    })
+    }
+
+    database.insert('users', user)
 
     return response.writeHead(201).end()
   }
