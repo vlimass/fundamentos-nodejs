fc4cecb16fc98efd977054e87ea3aa6e4173be1a
Vinícius Lima <villylss@gmail.com>
2023-10-19 23:11:28 -0300

    novas rotas adicionadas e aplicação finalizada

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import http from 'node:http'
 
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
 const server = http.createServer(async (request, response) =>  {
   const { method, url } = request
@@ -15,7 +16,10 @@ const server = http.createServer(async (request, response) =>  {
   if(route) {
     const routeParams = request.url.match(route.path)
 
-    console.log(routeParams)
+    const { query, ...params } = { ...routeParams.groups }
+
+    request.params = params
+    request.query = query ? extractQueryParams(query) : {}
       
     return route.handler(request, response)
   }
